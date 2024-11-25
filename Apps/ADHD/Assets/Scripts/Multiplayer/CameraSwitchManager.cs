45c97e24b51731e57457139b34a510343d782619
João Oliveira <1191457@isep.ipp.pt>
2024-11-25 23:51:21 +0000

    (#2 & #42) VR setup in the project. Does not include previously created VR features.

---
+++ Apps/ADHD/Assets/Scripts/Multiplayer/CameraSwitchManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class CameraSwitchManager : MonoBehaviour
+{
+    public static CameraSwitchManager Instance { get; private set; }
+
+    private Camera pcCamera;
+    private GameObject vrRig;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // Subscribe to the sceneLoaded event
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        UpdateCameraAndCanvas();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        UpdateCameraAndCanvas();
+    }
+
+    public void UpdateCameraAndCanvas()
+    {
+        // OBtain the VR rig, the canvas and the main camera
+        pcCamera = Camera.main;
+        
+        GameObject vrRig = GameObject.Find("OVRCameraRigInteraction");
+        if (vrRig != null)
+        {
+            this.vrRig = vrRig;
+        }
+
+        canvas = FindObjectOfType<Canvas>();
+
+        // Switch settings
+        if (XRSettings.isDeviceActive)
+        {
+            SwitchToVR();
+        }
+        else
+        {
+            SwitchToPC();
+        }
+    }
+
+    private void SwitchToVR()
+    {
+        // Enable the vr rig c:
+        if (vrRig != null) vrRig.gameObject.SetActive(true);
+        if (pcCamera != null) pcCamera.gameObject.SetActive(true);
+
+        if (canvas != null)
+        {
+            canvas.renderMode = RenderMode.WorldSpace;
+            //canvas.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+        }
+    }
+
+    private void SwitchToPC()
+    {
+        // Disable the vr rig :c
+        if (vrRig != null) vrRig.gameObject.SetActive(false);
+        if (pcCamera != null) pcCamera.gameObject.SetActive(true);
+
+        if (canvas != null)
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
--- Apps/ADHD/Assets/Scripts/Multiplayer/DeckEditor/UI_DeckEditorController.cs
+++ Apps/ADHD/Assets/Scripts/Multiplayer/DeckEditor/UI_DeckEditorController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class UI_DeckEditorController : MonoBehaviour
 {
