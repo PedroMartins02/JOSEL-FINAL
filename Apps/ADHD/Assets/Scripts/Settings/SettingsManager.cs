eb4a5ee83a9cc29c568c9332c22d764759b01d45
Hasuru <hugo.as.silva25@gmail.com>
2024-11-28 21:29:46 +0000

    #37 Audio Volume is stored and changed by saving the audio settings

--- Apps/ADHD/Assets/Scripts/Settings/SettingsManager.cs
+++ Apps/ADHD/Assets/Scripts/Settings/SettingsManager.cs
@@ -6,79 +6,76 @@ using UnityEngine.Rendering;
 
 public class SettingsManager : MonoBehaviour
 {
-    public static SettingsManager Instance;
+  public static SettingsManager Instance;
+  public AudioMixerManager audioMixerManager;
 
-    [Header("Settings")]
-    public bool isFullScreen;
-    public int resolutionIndex;
-    public int masterVolumeIndex;
-    public int musicVolumeIndex;
-    public int sfxVolumeIndex;
+  [Header("Settings")]
+  public bool isFullScreen;
+  public int resolutionIndex;
+  public int masterVolumeIndex;
+  public int musicVolumeIndex;
+  public int sfxVolumeIndex;
 
-    private void Awake()
+  private void Awake()
+  {
+    if (Instance == null)
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            LoadSettings();
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadSettings();
+    }
+    else
+    {
+        Destroy(gameObject);
+    }
+  }
+
+  public void LoadSettings()
+  {
+    isFullScreen = PlayerPrefs.GetInt("Settings FullScreen", 1) == 1;
+    resolutionIndex = PlayerPrefs.GetInt("Settings Resolution", GetCurrentResolutionIndex());
+    masterVolumeIndex = PlayerPrefs.GetInt("Settings Master Volume", 10);
+    musicVolumeIndex = PlayerPrefs.GetInt("Settings Music Volume", 10);
+    sfxVolumeIndex = PlayerPrefs.GetInt("Settings SFX Volume", 10);
+
+    ApplySettings();
+  }
+
+  public void SaveSettings()
+  {
+    PlayerPrefs.SetInt("Settings FullScreen", isFullScreen ? 1 : 0);
+    PlayerPrefs.SetInt("Settings Resolution", resolutionIndex);
+    PlayerPrefs.SetInt("Settings Master Volume", masterVolumeIndex);
+    PlayerPrefs.SetInt("Settings Music Volume", musicVolumeIndex);
+    PlayerPrefs.SetInt("Settings SFX Volume", sfxVolumeIndex);
+    PlayerPrefs.Save();
+  }
+
+  public void ApplySettings()
+  {
+    Screen.fullScreen = isFullScreen;
+    Resolution[] resolutions = Screen.resolutions;
+
+    if (resolutionIndex >= 0 && resolutionIndex < resolutions.Length)
+    {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, isFullScreen);
+    }
+    else
+    {
+        Debug.Log("Invalid resolution index found: " + resolutionIndex);
     }
 
-    public void LoadSettings()
+	audioMixerManager.UpdateVolumeLevels();
+  }
+
+  public int GetCurrentResolutionIndex()
+  {
+    Resolution[] resolutions = Screen.resolutions;
+    for (int i = 0; i < resolutions.Length; i++)
     {
-        //Debug.Log("Loading settings...");
-        isFullScreen = PlayerPrefs.GetInt("Settings FullScreen", 1) == 1;
-        resolutionIndex = PlayerPrefs.GetInt("Settings Resolution", GetCurrentResolutionIndex());
-        masterVolumeIndex = PlayerPrefs.GetInt("Settings Master Volume", 10);
-        musicVolumeIndex = PlayerPrefs.GetInt("Settings Music Volume", 10);
-        sfxVolumeIndex = PlayerPrefs.GetInt("Settings SFX Volume", 10);
-
-        ApplySettings();
+        if (resolutions[i].Equals(Screen.currentResolution))
+            return i;
     }
-
-    public void SaveSettings()
-    {
-        //Debug.Log("Saving settings...");
-        PlayerPrefs.SetInt("Settings FullScreen", isFullScreen ? 1 : 0);
-        PlayerPrefs.SetInt("Settings Resolution", resolutionIndex);
-        PlayerPrefs.SetInt("Settings Master Volume", masterVolumeIndex);
-        PlayerPrefs.SetInt("Settings Music Volume", musicVolumeIndex);
-        PlayerPrefs.SetInt("Settings SFX Volume", sfxVolumeIndex);
-        PlayerPrefs.Save();
-    }
-
-    public void ApplySettings()
-    {
-        //Debug.Log("Applying settings...");
-        // Set Resolution for desktop environments
-        Screen.fullScreen = isFullScreen;
-        Resolution[] resolutions = Screen.resolutions;
-
-        if (resolutionIndex >= 0 && resolutionIndex < resolutions.Length)
-        {
-            Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, isFullScreen);
-        }
-        else
-        {
-            Debug.Log("Invalid resolution index found: " + resolutionIndex);
-        }
-
-        // Set game Volumes
-    }
-
-    public int GetCurrentResolutionIndex()
-    {
-        Resolution[] resolutions = Screen.resolutions;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            if (resolutions[i].Equals(Screen.currentResolution))
-                return i;
-        }
-        return -1;
-    }
+    return -1;
+  }
 }
