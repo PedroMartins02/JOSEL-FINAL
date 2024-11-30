d287ce8021590c55e6af6263136c97bf79e0b4a0
João Oliveira <1191457@isep.ipp.pt>
2024-11-30 01:23:07 +0000

    (#45) Virtual keyboard changes.

--- Apps/ADHD/Assets/Scripts/Multiplayer/OVRAuthenticationInputHandler.cs
+++ Apps/ADHD/Assets/Scripts/Multiplayer/OVRAuthenticationInputHandler.cs
@@ -10,11 +10,14 @@ using UnityEngine.UI;
 
 public class OVRAuthenticationInputHandler : OVRVirtualKeyboard.AbstractTextHandler
 {
+    public static OVRAuthenticationInputHandler Instance { get; private set; }
+
+
     /// <summary>
     /// Set an input field to connect to the Virtual Keyboard with the Unity Inspector
     /// </summary>
-    [SerializeField] private TMP_InputField usernameField;
-    [SerializeField] private TMP_InputField passwordField;
+    [SerializeField]
+    private TMP_InputField inputField;
 
     private bool _isSelected;
 
@@ -23,76 +26,93 @@ public class OVRAuthenticationInputHandler : OVRVirtualKeyboard.AbstractTextHand
     /// </summary>
     public TMP_InputField InputField
     {
-        get => usernameField;
+        get => inputField;
         set
         {
-            if (value == usernameField)
+            if (value == inputField)
             {
                 return;
             }
-            if (usernameField)
+            if (inputField)
             {
-                usernameField.onValueChanged.RemoveListener(ProxyOnValueChanged);
+                inputField.onValueChanged.RemoveListener(ProxyOnValueChanged);
             }
-            usernameField = value;
-            if (usernameField)
+            inputField = value;
+            if (inputField)
             {
-                usernameField.onValueChanged.AddListener(ProxyOnValueChanged);
+                inputField.onValueChanged.AddListener(ProxyOnValueChanged);
             }
             OnTextChanged?.Invoke(Text);
         }
     }
 
-    public override Action<string> OnTextChanged { get; set; }
-
-    public override string Text => usernameField ? usernameField.text : string.Empty;
-
-    public override bool SubmitOnEnter => usernameField;
-
-    public override bool IsFocused => usernameField && usernameField.isFocused;
-
-    public override void Submit()
+    protected void Awake()
     {
-        if (!usernameField)
-        {
-            return;
-        }
-        usernameField.onEndEdit.Invoke(usernameField.text);
-    }
-
-    public override void AppendText(string s)
-    {
-        if (!usernameField)
-        {
-            return;
-        }
-        usernameField.text += s;
-    }
-
-    public override void ApplyBackspace()
-    {
-        if (!usernameField || string.IsNullOrEmpty(usernameField.text))
-        {
-            return;
-        }
-        usernameField.text = Text.Substring(0, Text.Length - 1);
-    }
-
-    public override void MoveTextEnd()
-    {
-        if (!usernameField)
-        {
-            return;
-        }
-        usernameField.MoveTextEnd(false);
+        Instance = this;
     }
 
     protected void Start()
     {
-        if (usernameField)
+        if (inputField)
         {
-            usernameField.onValueChanged.AddListener(ProxyOnValueChanged);
+            inputField.onValueChanged.AddListener(ProxyOnValueChanged);
         }
+        HideKeyboard();
+    }
+
+    public void ShowKeyboard()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void HideKeyboard()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public override Action<string> OnTextChanged { get; set; }
+
+    public override string Text => inputField ? inputField.text : string.Empty;
+
+    public override bool SubmitOnEnter => inputField && inputField.lineType != TMP_InputField.LineType.MultiLineNewline;
+
+    public override bool IsFocused => inputField && inputField.isFocused;
+
+    public override void Submit()
+    {
+        if (!inputField)
+        {
+            return;
+        }
+        inputField.onEndEdit.Invoke(inputField.text);
+        HideKeyboard();
+    }
+
+    public override void AppendText(string s)
+    {
+        if (!inputField)
+        {
+            return;
+        }
+        inputField.text += s;
+    }
+
+    public override void ApplyBackspace()
+    {
+        if (!inputField || string.IsNullOrEmpty(inputField.text))
+        {
+            return;
+        }
+        inputField.text = Text.Substring(0, Text.Length - 1);
+    }
+
+    public override void MoveTextEnd()
+    {
+        if (!inputField)
+        {
+            return;
+        }
+        inputField.MoveTextEnd(false);
     }
 
     protected void ProxyOnValueChanged(string arg0)
